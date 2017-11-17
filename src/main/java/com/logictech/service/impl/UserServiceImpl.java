dfbccf811568553e28efa59d8af238d58700ac54
Nowforpasserby <bb-cc@foxmail.com>
2017-11-17 21:28:35 +0800

    调用自己定义的共同接口

--- src/main/java/com/logictech/service/impl/UserServiceImpl.java
+++ src/main/java/com/logictech/service/impl/UserServiceImpl.java
@@ -29,7 +29,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer addUser(UserDTO userDTO) throws Exception {
-        return userMapper.insertSelective(userDTO);
+        return userMapper.mInsert(userDTO);
     }
 
     @Override
