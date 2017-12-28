9344eaf839ba523f457316fbce25d0b29e606e05
Nowforpasserby <bb-cc@foxmail.com>
2017-12-28 09:51:04 +0800

    补全 hibernate 验证注解

--- src/main/java/com/logictech/validator/MoneyValidator.java
+++ src/main/java/com/logictech/validator/MoneyValidator.java
@@ -1,9 +1,31 @@
 package com.logictech.validator;
 
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
 /**
  * @author JG.Hannibal
  * @since 2017/12/27 22:13
  */
-public class MoneyValidator {
+public class MoneyValidator implements ConstraintValidator<Money, String> {
+
+    private String regexp;
+
+    @Override
+    public void initialize(Money constraintAnnotation) {
+        this.regexp = constraintAnnotation.regexp();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+        if (value.matches(regexp)) {
+            return true;
+        }
+        return false;
+    }
+
 }
     
